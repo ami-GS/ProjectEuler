6be0dba93d35b1d62d7220e6983c9ae8148b306f
ami-GS <1991.daiki@gmail.com>
2018-01-21 11:21:51 +0900

    string based solution is faster

--- prob_36.go
+++ prob_36.go
@@ -44,6 +44,41 @@ func calc() {
 	fmt.Println(ans)
 }
 
+func calc2() {
+	// use strconv
+	ans := 0
+	for i := 1; i < 1000000; i += 2 {
+		iStr := strconv.Itoa(i)
+
+		missDec := false
+		for j := 0; j < len(iStr); j++ {
+			if iStr[j] != iStr[len(iStr)-1-j] {
+				missDec = true
+				break
+			}
+		}
+		if missDec {
+			continue
+		}
+
+		missBin := false
+		iBin := strconv.FormatInt(int64(i), 2)
+		for j := 0; j < len(iBin)/2; j++ {
+			if iBin[len(iBin)-j-1] != iBin[j] {
+				missBin = true
+				break
+			}
+		}
+		if missBin {
+			continue
+		}
+		fmt.Println(i, iBin)
+		ans += i
+	}
+	fmt.Println(ans)
+}
+
 func main() {
 	calc()
+	calc2()
 }
